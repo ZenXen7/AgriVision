4aef0cc74bbe7d95060802b049fef6b6e516a95d
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-16 14:11:26 +0800

    Added Proper Files needed to application

---
+++ client/app/(tabs)/scan.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Scan = () => {
+  return (
+    <View>
+        {/* opens a card tells user how to take proper pictures for scan  */}
+        
+      <Text>Scan</Text>
+    </View>
+  )
+}
+
+export default Scan
